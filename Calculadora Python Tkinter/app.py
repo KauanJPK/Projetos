1c7be7b0f158af67d20157112f0de159dc5e7b86
Kauan <kauan.pereiradev@gmail.com>
2025-07-29 22:41:49 -0300

    Calculadora em Tkinter

---
+++ Calculadora Python Tkinter/app.py
@@ -0,0 +1,74 @@
+from tkinter import *
+
+
+class App:
+    def __init__(self, master=None):
+
+        self.widget1 = Frame(master, bg="#EF8FFA")
+        self.widget1.pack()
+
+        self.ecra = Label(master, width= 30, height= 2, bg='#B509C8')
+        self.ecra['text'] = ''
+        self.ecra.pack(side="top")
+
+        self.numeros_Frame = Frame(master, width= 25, height= 2)
+        self.numeros_Frame["bg"] = 'White'
+        self.numeros_Frame.pack()
+
+        botoes = [
+            "1", "2", "3",
+            "4", "5", "6",
+            "7", "8", "9",
+            "0", "/",
+            "*", "+", "-",
+            "C", "="
+        ]
+        for i, valor in enumerate(botoes):
+            row = i // 4
+            col = i % 4
+            if valor == "C":
+                btn = Button(self.numeros_Frame, text=valor, width=5, height=2, fg="white", background="red")
+                btn.bind('<Button-1>', self.apagar)
+            if valor == "=":
+                btn = Button(self.numeros_Frame, text=valor, width=5, height=2, bg="green", fg="white")
+                btn.bind('<Button-1>', self.calcular)
+            else:
+                btn = Button(self.numeros_Frame, text=valor, width=5, height=2, bg="#E027F5", fg="white")
+                btn.bind('<Button-1>', self.adicionar)
+            btn.grid(row=row, column=col, padx=5, pady=5)
+            
+
+    def criarBotao(self, valor):
+        botao = Button(self.numeros_Frame, text=valor, width=5, height=2)
+        botao.bind('<Button-1>', self.adicionar)
+        botao.pack(side="left")
+            
+    def adicionar(self, event):
+        texto_atual = self.ecra['text']
+        novo_texto = texto_atual + event.widget['text']
+        self.ecra['text'] = novo_texto
+
+    def apagar(self, event):
+        self.ecra['text'] = ''
+
+    def calcular(self, event):
+        calculo = self.ecra['text']
+        if "/" or "*" or "+" or "-" in calculo:
+            calculado = eval(calculo)
+            self.ecra['text'] = calculado
+
+root = Tk()
+App(root)
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
